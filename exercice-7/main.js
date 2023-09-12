a40bbb91626c786147e882db792cb4f7de407cc7
Reubi0910 <esteban.morillo@divtec.ch>
2023-09-12 14:59:22 +0200

    fix exercice 7

--- exercice-7/main.js
+++ exercice-7/main.js
@@ -3,6 +3,7 @@ const nombres = [2, 3, 4, 5, 6, 7, 8, 9];
 
 for (let i = 0; i < nombres.length; i++) {
 	let diviseur = nombres[i];
+	let estUnfacteur = false;
 
 	if (DIVIDENDE % diviseur === 0) {
 		estUnfacteur = true;
